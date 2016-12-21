4bd7fd018ecc41a4070fe171ae46a0bb821cba52
upali <upali.choden@selise.ch>
2016-12-21 21:36:03 +0600

    new angularjs file

---
+++ app.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict'; //protecting us from making mistakes
+
+    angular.module('Namecalculator', [])
+        .controller('Namecalculatorcontroller',function ($scope) {
+            $scope.name="";
+         $scope.totalValue=0;
+          // $scope.sayhey=function () {
+          //     return "hello upali";
+          //   }
+         $scope.Displaynumeric=function () {
+             var Totalnamevalue=calnumericforstring($scope.name);
+             $scope.totalValue=Totalnamevalue;
+             
+         }
+         function calnumericforstring(string) {
+             var totstring=0;
+             for(var i=0;i<string.length;i++)
+             {
+                 totstring++;
+             }
+             return totstring;
+         }
+
+        });
+    
+})();
